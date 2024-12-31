bdbf6bb48d3ad8350f426ca399bb1b156a0fdf5f
Ali Dehkordi <alikh163@yahoo.com>
2024-12-31 22:05:43 +0330

    commit message

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <script setup>
-   import { ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-const name = ref("");
+import CompB from "@/components/CompB.vue";
 
-
+import CompA from "@/components/CompA.vue";
 </script>
-
-
 <template>
-  <input v-model="name" type="text">
-<div class="hi">hi {{ name }}</div>
-<HelloWorld/>
-</template>
-
-<!-- <style>
-.hi {
-  border: solid 1px rgb(216, 213, 24);
-  border-radius: 10px;
-  padding-left: 10px;
-  color: rgb(8, 14, 8);
-  background: yellow;
-}
-</style> -->
+  <div>App.vue</div>
+  <CompA/>
+  <CompB/>
+  </template>
